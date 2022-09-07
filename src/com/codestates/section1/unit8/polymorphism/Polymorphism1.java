6a57ccef23c695f12b971a2d2793af8d6beb2184
Eunyoung KANG <greenkey20@gmail.com>
2022-09-07 23:58:09 +0900

    feat: add study codes of polymorphism

---
+++ src/com/codestates/section1/unit8/polymorphism/Polymorphism1.java
@@ -0,0 +1,12 @@
+package com.codestates.section1.unit8.polymorphism;
+
+// 2022.9.7(수) 23h50
+public class Polymorphism1 {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
+
+class Person {
+
+}
