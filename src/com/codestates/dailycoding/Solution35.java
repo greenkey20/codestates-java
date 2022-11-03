cfc6471eb65fd411c8522a41cec4bd51195def88
Eunyoung KANG <greenkey20@gmail.com>
2022-11-04 02:45:52 +0900

    fix: update daily problem-solving exercise [quick sort]

--- src/com/codestates/dailycoding/Solution35.java
+++ src/com/codestates/dailycoding/Solution35.java
@@ -11,7 +11,8 @@ public class Solution35 {
         */
 
         // 문제를 더 이상 쪼갤 수 없을 때
-        if (arr.length <= 1) {
+        /*
+        if (arr.length <= 1) { // v3 = quickSort()의 인자로 배열의 길이가 0인 경우가 올 수 있고 이 때도 흐름 끊어야 하는데 그냥 넘어가서 illegal argument exception 발생한 바, 이 조건식 수정
             return arr;
         }
 
@@ -19,6 +20,17 @@ public class Solution35 {
         pivot = partition(arr);
         quickSort(Arrays.copyOfRange(arr, 0, pivot));
         quickSort(Arrays.copyOfRange(arr, pivot + 1, arr.length));
+        return arr;
+         */
+
+        // 2022.11.4(금) 1h55 내 로직으로는 partition() 1번만 호출되고 정렬이 종료되는 것 같음(하지만 현재 나는 이 재귀의 동작을 정확히 파악하지 못함) -> v4 knou 교과서 알고리즘 그대로 수정해봄 = 그래도 결과 동일
+        if (arr.length > 1) {
+            int pivot = 0;
+            pivot = partition(arr);
+            quickSort(Arrays.copyOfRange(arr, 0, pivot));
+            quickSort(Arrays.copyOfRange(arr, pivot + 1, arr.length));
+        }
+
         return arr;
     }
 
@@ -31,7 +43,7 @@ public class Solution35 {
             while (right > 0 && arr[right] >= arr[0]) right--; // pivot보다 작은 수 찾기; arr[right]에서 ArrayIndexOutOfBoundsException
 
             if (left < right) {
-                int temp = arr[right];
+                int temp = arr[right]; // v2 = arr[right/left]가 아니라 right/left(인덱스 값) 자체를 교환해서 ArrayIndexOutOfBoundsException 발생한 바, 이 부분 수정
                 arr[right] = arr[left];
                 arr[left] = temp;
             } else {
