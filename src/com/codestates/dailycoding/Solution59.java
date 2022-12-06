95fe64f924ecad31d953ded8aaeb259dd0f20421
Eunyoung KANG <greenkey20@gmail.com>
2022-12-06 10:31:37 +0900

    feat: add daily problem-solving exercise [palindrome]

---
+++ src/com/codestates/dailycoding/Solution59.java
@@ -0,0 +1,20 @@
+package com.codestates.dailycoding;
+
+public class Solution59 {
+    // 2022.12.6(화) 9h50 -> 10h 제출하고 레퍼런스 코드 공부하기로 함 -> 10h30 레퍼런스 코드 읽어봤는데, 잘 이해 안 됨..ㅠㅠ
+    public int longestPalindrome(String str) {
+        /* 입력받은 문자열 str의 부분 문자열 중 가장 긴 palindrome/회문(데이터를 앞에서 뒤로 + 뒤에서 앞으로 조회한 결과가 동일, 거꾸로 읽어도 제대로 읽는 것과 같은)의 길이를 리턴
+        str의 부분 문자열은 자기 자신을 포함 + 공백도 palindrome에 포함될 수 있음
+        나의 질문 = 대/소문자 구분하려나..?
+
+        tips) 부분 문자열 중 가장 긴 회문을 계산하는 효율적인 알고리즘(O(N^2))이 존재 -> 쉽지 않기 때문에 바로 레퍼런스 코드를 보고 이해하는 데 집중하기
+
+        흑,, 나는 아직 이런 문제 못 풀겠다.. 어떻게 사고력(이런 문제 풀이에 필요한 알고리즘을 생각해내는 능력)을 향상시킬 수 있을까? 코플릿 section2 및 데일리코딩 앞쪽 문제부터 제대로 학습하고 와야지..
+        */
+        // 입력받은 문자열 str을 모두 소문자로 바꾼 뒤 시작
+        String strInLowerCase = str.toLowerCase();
+
+        int lengthOfLongestPalindrome = 0;
+        return lengthOfLongestPalindrome;
+    }
+}
