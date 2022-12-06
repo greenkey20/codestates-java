c2ef649f457d1c37e0f4df2e13783c53c686c0bb
Eunyoung KANG <greenkey20@gmail.com>
2022-12-06 23:59:31 +0900

    feat: add graph traversal algorithm exercises

--- src/com/codestates/dailycoding/Solution27.java
+++ src/com/codestates/dailycoding/Solution27.java
@@ -1,15 +1,35 @@
 package com.codestates.dailycoding;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution27 {
     // 2022.10.20(목) 11h
-    public ArrayList<String> bfs(Tree node) {
+    public static ArrayList<String> bfs(Tree node) {
         // 임의의 tree를 구성하는 노드 중 하나의 Node 객체를 입력받아,
         // 해당 노드를 시작으로 너비 우선 탐색(BFS, Breadth First Search)을 함 -> 탐색되는 순서대로 노드의 값이 저장된 배열을 리턴
         // 주어진 Tree 자료구조 클래스는 DFS 문제에서와 동일
         // 어떻게 구현할지 잘 모르겠어서 구글링, 알고리즘 책 등 공부
-        return null;
+
+        // 2022.12.6(화) 23h50 구현
+        Queue<Tree> queue = new LinkedList<>();
+//        List<Boolean> visited = new ArrayList<>();
+        queue.add(node);
+
+        ArrayList<String> values = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            Tree current = queue.poll();
+
+            values.add(current.getValue());
+
+            if (current.getChildrenNode() != null) {
+                queue.addAll(current.getChildrenNode());
+            }
+        }
+
+        return values;
     }
 
     // 아래 클래스의 내용은 수정하지 말아야 합니다.
@@ -35,5 +55,22 @@ public class Solution27 {
             children.add(node);
             return children.get(children.size() - 1);
         }
+    }
+
+    // 2022.12.6(화) 23h30
+    public static void main(String[] args) {
+        Solution27.Tree root = new Solution27.Tree("1");
+        Solution27.Tree rootChild1 = root.addChildNode(new Solution27.Tree("2"));
+        Solution27.Tree rootChild2 = root.addChildNode(new Solution27.Tree("3"));
+        Solution27.Tree leaf1 = rootChild1.addChildNode(new Solution27.Tree("4"));
+        Solution27.Tree leaf2 = rootChild1.addChildNode(new Solution27.Tree("5"));
+
+        ArrayList<String> output = bfs(root);
+        System.out.println(output);
+
+        leaf1.addChildNode(new Solution27.Tree("6"));
+        rootChild2.addChildNode(new Solution27.Tree("7"));
+        output = bfs(root);
+        System.out.println(output);
     }
 }
--- src/com/codestates/section2/unit2/graph/Graph12Draft2.java
+++ src/com/codestates/section2/unit2/graph/Graph12Draft2.java
@@ -2,20 +2,80 @@ package com.codestates.section2.unit2.graph;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 // 2022.11.6(일) 23h55
 public class Graph12Draft2 {
     public int connectedVertices(int[][] edges) {
         final int[] bigger = {0};
 
+        // 최대 vertex 찾기
         Arrays.stream(edges).forEach(data -> {
             int currentBigger = Arrays.stream(data).boxed().max(Comparator.comparing(i -> i)).orElse(0);
             if (bigger[0] < currentBigger) bigger[0] = currentBigger;
         });
 
+        // (최대 vertex + 1) 크기로 인접행렬 만들기
         int[][] adjArray = new int[bigger[0] + 1][bigger[0] + 1];
 
+        // 2022.12.6(화) 10h30 이어서 학습
+        // 간선이 있는 vertex끼리 인접행렬 상 연결
+        for (int i = 0; i < edges.length; i++) {
+            int from = edges[i][0];
+            int to = edges[i][1];
+            adjArray[from][to] = 1;
+            adjArray[to][from] = 1;
+        }
+
+        boolean[] visited = new boolean[bigger[0] + 1];
         int count = 0;
+
+        for (int vertex = 0; vertex < visited.length; vertex++) {
+            if (!visited[vertex]) {
+                count++;
+
+                // 방법1) bfs
+                visited = bfs_array(adjArray, vertex, visited);
+
+                // 방법2) dfs
+//                visited = dfs_array(adjArray, vertex, visited);
+            }
+        }
+
         return count;
+    }
+
+    public boolean[] bfs_array(int[][] adjArray, int vertex, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(vertex);
+        visited[vertex] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            // 현재 선택된 정점에 1차로 연결된 모든 정점들을 queue에 담음
+            for (int i = 0; i < adjArray[current].length; i++) {
+                if (adjArray[current][i] == 1 && !visited[i]) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+
+        return visited;
+    }
+
+    public boolean[] dfs_array(int[][] adjArray, int vertex, boolean[] visited) {
+        visited[vertex] = true;
+
+        for (int i = 0; i < adjArray.length; i++) {
+            if (adjArray[vertex][i] == 1 && !visited[i]) {
+                // 현재 선택된 정점에서 1차로 연결된 정점에 대해, 그 정점이 1차로 연결된 정점을 찾아가고..(깊게 파고 들어감?)
+                dfs_array(adjArray, i, visited);
+            }
+        }
+
+        return visited;
     }
 }
