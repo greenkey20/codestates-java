fb097c324eb54b611cda9e61563515ee8458ca3f
Eunyoung KANG <greenkey20@gmail.com>
2023-07-05 19:42:35 +0900

    feat: add study codes of generic

--- src/com/codestates/section1/unit9/genericExercise/GenericConcept.java
+++ src/com/codestates/section1/unit9/genericExercise/GenericConcept.java
@@ -16,6 +16,14 @@ public class GenericConcept {
         Basket<Flower> flowerBasket = new Basket<>(); // Basket의 인스턴스인 flowerBasket의 멤버변수의 타입을 Flower로 지정
         flowerBasket.setItem(new Rose()); // Rose는 Flower를 상속받는, Flower의 하위/자식 클래스 -> flowerBasket의 멤버변수 item으로 할당 가능 -> 다형성 적용해서 이렇게 타입 매개변수로 적용 가능
 //        flowerBasket.setItem(new RosePasta()); // RosePasta는 Flower와 아무 관계 없는 클래스 -> flowerBasket 객체에는 RosePasta로 멤버변수를 set하는 setter가 없는 바, 에러
+
+        Basket<String> basket = new Basket<>();
+        basket.<Integer>add(10); // 제네릭 메서드 호출 시 제네릭 메서드의 타입 지정
+        basket.add("String"); // 타입 지정 생략도 가능
+
+
+        Basket<Rose> roseBasket = new Basket<>();
+        Basket<RosePasta> rosePastaBasket = new Basket<>();
     }
 
 }
@@ -38,7 +46,8 @@ class BasketString {
 }
 
 // 제네릭 클래스 예시1 = 모든 타입의 데이터를 저장할 수 있는 인스턴스 생성 가능
-class Basket<T> {
+// 2023.7.5(수) 15h20 타입 매개변수 선언 시 아래와 같이 코드 작성하면 Basket 클래스 인스턴스화 시 (1) Flower의 하위 클래스 ou (2) 특정 인터페이스를 구현한 클래스만 ou (3) 특정 클래스 상속받으며 특정 인터페이스 구현한 클래스만 타입으로 지정 가능하도록 제한
+class Basket<T /*extends Flower & Plant*/> {
     private T item;
 
     public Basket() {
@@ -54,6 +63,20 @@ class Basket<T> {
 
     public void setItem(T item) {
         this.item = item;
+    }
+
+    // 2023.7.5(수) 18h50 클래스 내부의 특정 메서드만 제네릭으로 선언 = 제네릭 메서드
+    public <T> void add(T element) {
+
+    }
+
+    // static 메서드에서도 제네릭 타입 매개변수 사용 가능 <- 메서드에 선언된 제네릭 타입은 지역 변수 선언한 것과 같음, 이 타입 매개변수는 메서드 내에서만 지역적으로 사용됨
+    public static <T> int setPrice(T element) {
+        return 0;
+    }
+
+    public <T> void getPrint(T item) {
+        System.out.println(item.equals("Tromm KANG"));
     }
 }
 
@@ -89,7 +112,7 @@ class Member<K, V> {
 class InfoMap<K, V> {
     private K key;
     private V value;
-//    static V value;
+//    static V value; // cannot be referenced from a static context = 클래스 변수에 서로 다른 타입 매개변수를 사용해서 인스턴스를 만들게 되면 클래스 변수의 타입이 서로 달라져서 클래스 변수를 통해 같은 변수를 공유하는 게 아닌 것이 됨 -> static 변수에는 타입 매개변수 사용 불가능
 
     public InfoMap(K key, V value) {
         this.key = key;
@@ -151,14 +174,19 @@ class Fruit {
 }
 
 // 제네릭 클래스 사용 시 다형성 적용 가능
-class Flower {
+class Flower implements Plant {
 
 }
 
-class Rose extends Flower {
+class Rose extends Flower implements Plant {
 
 }
 
 class RosePasta {
+
+}
+
+// 2023.7.5(수) 15h20
+interface Plant {
 
 }
---
+++ src/com/codestates/section1/unit9/genericExercise/WildCardConcept.java
@@ -0,0 +1,110 @@
+package com.codestates.section1.unit9.genericExercise;
+
+import static com.codestates.section1.unit8.sandwichprincess.common.Utils.printBoldLine;
+import static com.codestates.section1.unit8.sandwichprincess.common.Utils.printLine;
+
+// 2023.7.5(수) 19h10
+public class WildCardConcept {
+    public static void main(String[] args) {
+        PhoneFunction.call(new User<Phone>(new Phone()));
+        PhoneFunction.call(new User<IPhone>(new IPhone()));
+        PhoneFunction.call(new User<Galaxy>(new Galaxy()));
+        PhoneFunction.call(new User<IPhone12Pro>(new IPhone12Pro()));
+        PhoneFunction.call(new User<IphoneMini>(new IphoneMini()));
+        PhoneFunction.call(new User<S22>(new S22()));
+        PhoneFunction.call(new User<ZFlip3>(new ZFlip3()));
+
+        printBoldLine();
+
+//        PhoneFunction.faceId(new User<Phone>(new Phone()));
+        PhoneFunction.faceId(new User<IPhone>(new IPhone()));
+        PhoneFunction.faceId(new User<IPhone12Pro>(new IPhone12Pro()));
+        PhoneFunction.faceId(new User<IphoneMini>(new IphoneMini()));
+//        PhoneFunction.faceId(new User<Galaxy>(new Galaxy()));
+//        PhoneFunction.faceId(new User<S22>(new S22()));
+//        PhoneFunction.faceId(new User<ZFlip3>(new ZFlip3()));
+
+        printBoldLine();
+
+//        PhoneFunction.samsungPay(new User<Phone>(new Phone()));
+//        PhoneFunction.samsungPay(new User<IPhone>(new IPhone()));
+//        PhoneFunction.samsungPay(new User<IPhone12Pro>(new IPhone12Pro()));
+//        PhoneFunction.samsungPay(new User<IphoneMini>(new IphoneMini()));
+        PhoneFunction.samsungPay(new User<Galaxy>(new Galaxy()));
+        PhoneFunction.samsungPay(new User<S22>(new S22()));
+        PhoneFunction.samsungPay(new User<ZFlip3>(new ZFlip3()));
+
+        printBoldLine();
+
+        PhoneFunction.recordVoice(new User<Phone>(new Phone()));
+//        PhoneFunction.recordVoice(new User<IPhone>(new IPhone()));
+//        PhoneFunction.recordVoice(new User<IPhone12Pro>(new IPhone12Pro()));
+//        PhoneFunction.recordVoice(new User<IphoneMini>(new IphoneMini()));
+        PhoneFunction.recordVoice(new User<Galaxy>(new Galaxy()));
+//        PhoneFunction.recordVoice(new User<S22>(new S22()));
+//        PhoneFunction.recordVoice(new User<ZFlip3>(new ZFlip3()));
+    }
+}
+
+class Phone {
+}
+
+class IPhone extends Phone {
+}
+
+class Galaxy extends Phone {
+}
+
+class IPhone12Pro extends IPhone {
+
+}
+
+class IphoneMini extends IPhone {
+}
+
+class S22 extends Galaxy {
+
+}
+
+class ZFlip3 extends Galaxy {
+
+}
+
+class User<T> {
+    public T phone;
+
+    public User(T phone) {
+        this.phone = phone;
+    }
+}
+
+class PhoneFunction {
+    public static void call(User<? extends Phone> user) {
+        printLine();
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
+        System.out.println("모든 Phone은 통화할 수 있음");
+    }
+
+    public static void faceId(User<? extends IPhone> user) {
+        printLine();
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
+        System.out.println("IPhone만 Face ID를 사용할 수 있음");
+    }
+
+    public static void samsungPay(User<? extends Galaxy> user) {
+        printLine();
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
+        System.out.println("Galaxy만 삼성 페이를 사용할 수 있음");
+    }
+
+    // 상속 계층도에서 Galaxy 및 Galaxy보다 위에 있는 상위 클래스만 타입으로 지정 가능
+    public static void recordVoice(User<? super Galaxy> user) {
+        printLine();
+        System.out.println("user.phone = " + user.phone.getClass().getSimpleName());
+        System.out.println("안드로이드 폰에서만 통화 녹음이 가능함");
+    }
+}
+
+
+
+
