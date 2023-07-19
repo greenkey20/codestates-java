6a71ff8b75fdb00f744b3a5acadf24ae65b47e76
Eunyoung KANG <greenkey20@gmail.com>
2023-07-19 21:36:06 +0900

    comment: add comments

--- src/com/codestates/section1/unit6/condition/stuffedanimal/LessIfStatements.java
+++ src/com/codestates/section1/unit6/condition/stuffedanimal/LessIfStatements.java
@@ -1,135 +1,135 @@
-package com.codestates.section1.unit6.condition.stuffedanimal;
-
-// 2023.7.18(화) 저녁 help/advice by Eddie
-public class LessIfStatements {
-
-    // Here's an approach with "handlers" for each animal type.
-    class IStuffedAnimal {
-        void stuffBody();
-
-        void attachArms(Arms arms);
-
-        void stuffArms();
-
-        void attachLegs(Legs legs);
-
-        void stuffLegs();
-
-        void attachEars(Ears ears);
-
-        void stuffEars();
-    }
-
-    interface IStuffedAnimalMaker {
-        IStuffedAnimal makeBody();
-
-        IStuffedAnimal makeAndStuffBody();
-
-        IArms makeArms();
-
-        ILegs makeLegs();
-
-        IEars makeEars();
-
-        void makeAndAttachArmsToAnimal(IStuffedAnimal stuffedAnimal);
-
-        void makeAndAttachLegsToAnimal(IStuffedAnimal stuffedAnimal);
-
-        void makeAndAttachEarsToAnimal(IStuffedAnimal stuffedAnimal);
-    }
-
-    class BearMaker implements IStuffedAnimalMaker {
-        IStuffedAnimal makeBody() { ...return ...}
-
-        IStuffedAnimal makeAndStuffBody() { ...return ...}
-
-        IArms makeArms() { ...return ...}
-
-        ILegs makeLegs() { ...return ...}
-
-        IEars makeEars() { ...return ...}
-
-        @Override
-        public void makeAndAttachArmsToAnimal(IStuffedAnimal stuffedAnimal) {
-
-        }
-
-        @Override
-        public void makeAndAttachLegsToAnimal(IStuffedAnimal stuffedAnimal) {
-
-        }
-
-        @Override
-        public void makeAndAttachEarsToAnimal(IStuffedAnimal stuffedAnimal) {
-
-        }
-    }
-
-    enum AnimalType {
-        BEAR,
-        RABBIT,
-        BIRD
-    }
-
-    AnimalType determineType(Input input) {
-        if (input.ears == "round") {
-            return BEAR;
-        } else if (input.ears == "long") {
-            return RABBIT;
-        } else /*if (input.ears == "none")*/ {
-            return BIRD;
-        }
-    }
-
-    IStuffedAnimalMaker getMaker(AnimalType type, Input input) {
-        if (type == BEAR) {
-            return new BearMaker(input);
-        } else if (type == BEAR) {
-            return new RabbitMaker(input);
-        } else /*if (type == BIRD)*/ {
-            return new BirdMaker(input);
-        }
-    }
-
-    IStuffedAnimal makeStuffedAnimalWithIf(Input input) {
-        AnimalType type = determineType(input);
-        IStuffedAnimalMaker maker = getMaker(type, input);
-
-        // Body
-        IStuffedAnimal result = maker.makeBody();
-        result.StuffBody();
-
-        // Limbs
-        IArms arms = maker.makeArms();
-        result.attachArms(arms);
-        result.stuffArms();
-
-        ILegs legs = maker.makeLegs();
-        result.attachLegs(legs);
-        result.stuffLegs();
-
-        // Ears
-        IEars ears = maker.makeEars();
-        if (ears != null) {
-            result.attachEars(ears);
-            result.stuffEars();
-        }
-
-        return result;
-    }
-
-    // In the above example there's still an if-statement in makeStuffedAnimal. You
-// can change how the maker works to avoid that. Here I'm passing in a stuffed
-// animal to the methods and let them do all the work:
-    IStuffedAnimal makeStuffedAnimalWithoutIf(Input input) {
-        AnimalType type = determineType(input);
-        IStuffedAnimalMaker maker = getMaker(type, input);
-
-        IStuffedAnimal result = maker.makeAndStuffBody();
-        maker.makeAndAttachArmsToAnimal(result);
-        maker.makeAndAttachLegsToAnimal(result);
-        maker.makeAndAttachEarsToAnimal(result);
-
-        return result;
-    }
-}
+//package com.codestates.section1.unit6.condition.stuffedanimal;
+//
+//// 2023.7.18(화) 저녁 help/advice by Eddie
+//public class LessIfStatements {
+//
+//    // Here's an approach with "handlers" for each animal type.
+//    class IStuffedAnimal {
+//        void stuffBody();
+//
+//        void attachArms(Arms arms);
+//
+//        void stuffArms();
+//
+//        void attachLegs(Legs legs);
+//
+//        void stuffLegs();
+//
+//        void attachEars(Ears ears);
+//
+//        void stuffEars();
+//    }
+//
+//    interface IStuffedAnimalMaker {
+//        IStuffedAnimal makeBody();
+//
+//        IStuffedAnimal makeAndStuffBody();
+//
+//        IArms makeArms();
+//
+//        ILegs makeLegs();
+//
+//        IEars makeEars();
+//
+//        void makeAndAttachArmsToAnimal(IStuffedAnimal stuffedAnimal);
+//
+//        void makeAndAttachLegsToAnimal(IStuffedAnimal stuffedAnimal);
+//
+//        void makeAndAttachEarsToAnimal(IStuffedAnimal stuffedAnimal);
+//    }
+//
+//    class BearMaker implements IStuffedAnimalMaker {
+//        IStuffedAnimal makeBody() { ...return ...}
+//
+//        IStuffedAnimal makeAndStuffBody() { ...return ...}
+//
+//        IArms makeArms() { ...return ...}
+//
+//        ILegs makeLegs() { ...return ...}
+//
+//        IEars makeEars() { ...return ...}
+//
+//        @Override
+//        public void makeAndAttachArmsToAnimal(IStuffedAnimal stuffedAnimal) {
+//
+//        }
+//
+//        @Override
+//        public void makeAndAttachLegsToAnimal(IStuffedAnimal stuffedAnimal) {
+//
+//        }
+//
+//        @Override
+//        public void makeAndAttachEarsToAnimal(IStuffedAnimal stuffedAnimal) {
+//
+//        }
+//    }
+//
+//    enum AnimalType {
+//        BEAR,
+//        RABBIT,
+//        BIRD
+//    }
+//
+//    AnimalType determineType(Input input) {
+//        if (input.ears == "round") {
+//            return BEAR;
+//        } else if (input.ears == "long") {
+//            return RABBIT;
+//        } else /*if (input.ears == "none")*/ {
+//            return BIRD;
+//        }
+//    }
+//
+//    IStuffedAnimalMaker getMaker(AnimalType type, Input input) {
+//        if (type == BEAR) {
+//            return new BearMaker(input);
+//        } else if (type == BEAR) {
+//            return new RabbitMaker(input);
+//        } else /*if (type == BIRD)*/ {
+//            return new BirdMaker(input);
+//        }
+//    }
+//
+//    IStuffedAnimal makeStuffedAnimalWithIf(Input input) {
+//        AnimalType type = determineType(input);
+//        IStuffedAnimalMaker maker = getMaker(type, input);
+//
+//        // Body
+//        IStuffedAnimal result = maker.makeBody();
+//        result.StuffBody();
+//
+//        // Limbs
+//        IArms arms = maker.makeArms();
+//        result.attachArms(arms);
+//        result.stuffArms();
+//
+//        ILegs legs = maker.makeLegs();
+//        result.attachLegs(legs);
+//        result.stuffLegs();
+//
+//        // Ears
+//        IEars ears = maker.makeEars();
+//        if (ears != null) {
+//            result.attachEars(ears);
+//            result.stuffEars();
+//        }
+//
+//        return result;
+//    }
+//
+//    // In the above example there's still an if-statement in makeStuffedAnimal. You
+//// can change how the maker works to avoid that. Here I'm passing in a stuffed
+//// animal to the methods and let them do all the work:
+//    IStuffedAnimal makeStuffedAnimalWithoutIf(Input input) {
+//        AnimalType type = determineType(input);
+//        IStuffedAnimalMaker maker = getMaker(type, input);
+//
+//        IStuffedAnimal result = maker.makeAndStuffBody();
+//        maker.makeAndAttachArmsToAnimal(result);
+//        maker.makeAndAttachLegsToAnimal(result);
+//        maker.makeAndAttachEarsToAnimal(result);
+//
+//        return result;
+//    }
+//}
--- src/com/codestates/section1/unit6/condition/stuffedanimal/MoreIfStatements.java
+++ src/com/codestates/section1/unit6/condition/stuffedanimal/MoreIfStatements.java
@@ -1,71 +1,71 @@
-package com.codestates.section1.unit6.condition.stuffedanimal;
-
-// 2023.7.18(화) 저녁 help/advice by Eddie
-public class MoreIfStatements {
-    // Here's an approach with a single method using if-statements.
-    class IStuffedAnimal { ...
-    }
-
-    class Bear extends LessIfStatements.IStuffedAnimal { ...
-    }
-
-    class Rabbit extends LessIfStatements.IStuffedAnimal { ...
-    }
-
-    class Bird extends LessIfStatements.IStuffedAnimal { ...
-    }
-
-    LessIfStatements.IStuffedAnimal makeStuffedAnimal(Input input) {
-        LessIfStatements.IStuffedAnimal result;
-
-        // Body
-        // This is just an example of how one could use input to instantiate the different classes.
-        if (input.ears == "round") {
-            result = new Bear();
-        } else if (input.ears == "long") {
-            result = new Rabbit();
-        } else if (input.ears == "none") {
-            result = new Bird();
-        }
-
-        result.stuffBody();
-
-        // Limbs
-        Arms arms;
-        Legs legs;
-
-        if (input.ears == "round") {
-            arms = new BearArms();
-            legs = new BearLegs();
-        } else if (input.ears == "long") {
-            arms = new RabbitArms();
-            legs = new RabbitLegs();
-        } else if (input.ears == "none") {
-            arms = new BirdWings();
-            legs = new BirdLegs();
-        }
-
-        result.attachArms(arms);
-        result.stuffArms();
-        result.attachLegs(legs);
-        result.stuffLegs();
-
-        // Ears
-        Ears ears;
-
-        if (input.ears == "round") {
-            ears = new BearEars();
-        } else if (input.ears == "long") {
-            ears = new RabbitEars();
-        } else if (input.ears == "none") {
-            ears = null;
-        }
-
-        if (ears != null) {
-            result.attachEars(ears);
-            result.stuffEars();
-        }
-
-        return result;
-    }
-}
+//package com.codestates.section1.unit6.condition.stuffedanimal;
+//
+//// 2023.7.18(화) 저녁 help/advice by Eddie
+//public class MoreIfStatements {
+//    // Here's an approach with a single method using if-statements.
+//    class IStuffedAnimal { ...
+//    }
+//
+//    class Bear extends LessIfStatements.IStuffedAnimal { ...
+//    }
+//
+//    class Rabbit extends LessIfStatements.IStuffedAnimal { ...
+//    }
+//
+//    class Bird extends LessIfStatements.IStuffedAnimal { ...
+//    }
+//
+//    LessIfStatements.IStuffedAnimal makeStuffedAnimal(Input input) {
+//        LessIfStatements.IStuffedAnimal result;
+//
+//        // Body
+//        // This is just an example of how one could use input to instantiate the different classes.
+//        if (input.ears == "round") {
+//            result = new Bear();
+//        } else if (input.ears == "long") {
+//            result = new Rabbit();
+//        } else if (input.ears == "none") {
+//            result = new Bird();
+//        }
+//
+//        result.stuffBody();
+//
+//        // Limbs
+//        Arms arms;
+//        Legs legs;
+//
+//        if (input.ears == "round") {
+//            arms = new BearArms();
+//            legs = new BearLegs();
+//        } else if (input.ears == "long") {
+//            arms = new RabbitArms();
+//            legs = new RabbitLegs();
+//        } else if (input.ears == "none") {
+//            arms = new BirdWings();
+//            legs = new BirdLegs();
+//        }
+//
+//        result.attachArms(arms);
+//        result.stuffArms();
+//        result.attachLegs(legs);
+//        result.stuffLegs();
+//
+//        // Ears
+//        Ears ears;
+//
+//        if (input.ears == "round") {
+//            ears = new BearEars();
+//        } else if (input.ears == "long") {
+//            ears = new RabbitEars();
+//        } else if (input.ears == "none") {
+//            ears = null;
+//        }
+//
+//        if (ears != null) {
+//            result.attachEars(ears);
+//            result.stuffEars();
+//        }
+//
+//        return result;
+//    }
+//}
