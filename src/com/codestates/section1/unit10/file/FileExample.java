b57f7838090e1cc248cf82fa938f82d2d60da3d4
Eunyoung KANG <greenkey20@gmail.com>
2023-07-20 16:55:08 +0900

    fix: update study codes of file

--- src/com/codestates/section1/unit10/file/FileExample.java
+++ src/com/codestates/section1/unit10/file/FileExample.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 // 2023.7.20(목) 16h40
 public class FileExample {
     public static void main(String[] args) throws IOException {
-        File file = new File("../codestates.txt"); // 이 파일이 존재하지 않아도 컴파일 에러가 발생하지는 않음 + File 인스턴스 생성이 곧 파일을 생성하는 건 아님
+        File file = new File("./", "newFileCodestates.txt"); // 이 파일이 존재하지 않아도 컴파일 에러가 발생하지는 않음 + File 인스턴스 생성이 곧 파일을 생성하는 건 아님
+        file.createNewFile();
 
-        System.out.println(file.getPath()); // ../codestates.txt
-        System.out.println(file.getParent()); // ..
-        System.out.println(file.getCanonicalPath()); // /Users/greenpianorabbit/codestates/codestates.txt
-        System.out.println(file.canWrite()); // false
+        System.out.println(file.getPath()); // ./newFileCodestates.txt
+        System.out.println(file.getParent()); // . -> 16h45 나의 질문 = .(현재 디렉토리) vs .. 어떤 의미이지?
+        System.out.println(file.getCanonicalPath()); // /Users/greenpianorabbit/codestates/codestates-java/newFileCodestates.txt
+        System.out.println(file.canWrite()); // true
     }
 }
