d6904fc41f9534bebcb200647ba1c82f4b172bb5
Eunyoung KANG <greenkey20@gmail.com>
2023-07-21 09:19:37 +0900

    comment: add comments

--- src/com/codestates/section2/unit3/Permutation6.java
+++ src/com/codestates/section2/unit3/Permutation6.java
@@ -28,15 +28,17 @@ public class Permutation6 {
             return null;
         }
 
-        Collections.sort(stuffList);
+        Collections.sort(stuffList); // testCase1 = [1,10,1111]
+//        System.out.println("stuffList = " + stuffList); // todo
 
-        ArrayList<Integer[]> results = new ArrayList<>();
-        boolean[] visited = new boolean[stuffList.size()];
-        Integer[] output = new Integer[choiceNum];
+        ArrayList<Integer[]> results = new ArrayList<>(); // [[choiceNum 2 길이의 정수 배열], [길이2 정수 배열], [길이2 정수 배열], ...]
+        boolean[] visited = new boolean[stuffList.size()]; // [false, false, false]
+        Integer[] output = new Integer[choiceNum]; // choiceNum = 2 -> [0, 0]
 
-        permutation(0, output, stuffList, visited, results);
-
-        return results;
+        permutation(0, output, stuffList, visited, results); // pm(0, [0, 0], [1,10,1111], [false, false, false], [[1,10], [1,1111], [10,1], [10,1111], [1111,1], [1111,10]])
+        return results; // 2023.7.21(금) 1h25 testCase1로 코드 흐름 다 돌고 왔는데, 그래도 이해 정확히 안 됨.. 그냥 이런 공식으로 외워야/습득해야 하나..? ㅠㅠ
+        // 가장 이해가 잘 안 되는 건, 재귀 아래에서 return 받아 위로 올라갈/자신을 호출한 곳으로 돌아갈 때 results는 계속 업데이트해서 올려보냈는데, visited, output은 딱히 업데이트 안 해서 올라갔다. 이게 잘못된 것인지, 맞는 것인지, 정확히 모르겠다.. ㅠㅠ
+        // reference 강의 마지막에서, 외울 필요는 없고, reference code 흐름 이해하면 된다고 하셨는데..
     }
 
     public void permutation(int depth, Integer[] output, ArrayList<Integer> stuffList, boolean[] visited, ArrayList<Integer[]> results) {
@@ -49,7 +51,7 @@ public class Permutation6 {
 
 //            System.out.println("results = " + results);
             for (int i = 0; i < results.size(); i++) {
-                System.out.println("copyOutput = " + Arrays.toString(copyOutput));
+                System.out.println("copyOutput = " + Arrays.toString(copyOutput)); // todo
             }
             return;
         }
